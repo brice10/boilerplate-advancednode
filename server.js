af1f2d7570c7438b069e03c7d1cbe1ec3d91093b
Arléon <arleonzemtsop@gmail.com>
2024-02-05 11:14:29 +0100

    To pass the step: Logging a User Out

--- server.js
+++ server.js
@@ -34,11 +34,17 @@ myDB(async client => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
-  app
-    .route('/profile')
-    .get(ensureAuthenticated, (req, res) => {
-      res.render('profile', { username: req.user.username });
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout();
+      res.redirect('/');
     });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
 
 }).catch(e => {
   app.route('/').get((req, res) => {
