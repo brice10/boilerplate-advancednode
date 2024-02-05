eb59f1a192bd3cf5539cf6e3146636d9448db649
Arléon <arleonzemtsop@gmail.com>
2024-02-05 11:09:22 +0100

    To pass the step: Create New Middleware

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const { ObjectID } = require('mongodb');
-const LocalStrategy = require('passport-local');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -23,57 +28,36 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  app.route('/').get((req, res) => {
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please log in',
-      showLogin: true
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+
+  app
+    .route('/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      res.render('profile');
     });
-  });
-
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/profile');
-  })
-
-  app.route('/profile').get((req,res) => {
-    res.render('profile');
-  })
-
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
-      return done(null, user);
-    });
-  }));
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
 
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-  
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log('Listening on port ' + PORT);
+  console.log('process.env.MONGO_URI ', process.env.MONGO_URI);
+  console.log('process.env.SESSION_SECRET ', process.env.SESSION_SECRET);
+  console.log('process.env.GITHUB_CLIENT_ID ', process.env.GITHUB_CLIENT_ID);
+  console.log('process.env.GITHUB_CLIENT_SECRET ', process.env.GITHUB_CLIENT_SECRET);
 });
