acf564cb6bf7992b5a961c08b73347fe8c44eebe
Arléon <arleonzemtsop@gmail.com>
2024-02-05 13:27:51 +0100

    Step: Authentication with Socket.IO

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ const auth = require('./auth.js');
 
 const app = express();
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -31,6 +28,37 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
@@ -39,7 +67,7 @@ myDB(async client => {
 
   let currentUsers = 0;
   io.on('connection', socket => {
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
     ++currentUsers;
     io.emit('user count', currentUsers);
 
