3ed95ef8f482dcc774c81094dcbadfb0e54bb59e
Arléon <arleonzemtsop@gmail.com>
2024-02-05 10:47:30 +0100

    To pass the step: Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ myDB(async client => {
 
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, null);
+      done(null, doc);
     });
   });
 }).catch(e => {
